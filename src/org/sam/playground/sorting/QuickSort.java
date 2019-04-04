6045898d481d87f484524f9578746d8370e80bbf
lvoinescu <voinescu_lucian@yahoo.com>
2019-04-04 22:15:46 +0300

    Add tail-recursion and no recursion variants for quick-sorting

--- src/org/sam/playground/sorting/QuickSort.java
+++ src/org/sam/playground/sorting/QuickSort.java
@@ -1,6 +1,7 @@
 package org.sam.playground.sorting;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * O(n log n) efficient sorting algorithm
@@ -9,9 +10,30 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
+        long start;
+        long end;
+
         long[] numbers = new long[]{1L, 4L, 6L, 2L, 7L, 3L, 9L, 4L};
+        start = System.nanoTime();
         quickSort(numbers);
+        end = System.nanoTime();
         Arrays.stream(numbers).forEach(System.out::println);
+
+        System.out.println("Sorted in " + (end - start));
+
+        long[] numbers2 = new long[]{1L, 4L, 6L, 2L, 7L, 3L, 9L, 4L};
+        start = System.nanoTime();
+        quickSortTailRecursion(numbers2);
+        end = System.nanoTime();
+        Arrays.stream(numbers2).forEach(System.out::println);
+        System.out.println("Sorted in " + (end - start));
+
+        long[] numbers3 = new long[]{1L, 4L, 6L, 2L, 7L, 3L, 9L, 4L};
+        start = System.nanoTime();
+        quickSortNoRecursion(numbers3);
+        end = System.nanoTime();
+        Arrays.stream(numbers3).forEach(System.out::println);
+        System.out.println("Sorted in " + (end - start));
     }
 
     private static void quickSort(long[] numbers) {
@@ -24,6 +46,60 @@ public class QuickSort {
             pivot = processPartition(numbers, startIndex, endIndex, pivot);
             quickSort(numbers, startIndex, pivot - 1);
             quickSort(numbers, pivot + 1, endIndex);
+        }
+    }
+
+    private static void quickSortTailRecursion(long[] numbers) {
+        quickSortTailRecursion(numbers, 0, numbers.length - 1);
+    }
+
+    private static void quickSortTailRecursion(long[] numbers, int startIndex, int endIndex) {
+        int pivot = choosePivot(startIndex, endIndex);
+        int auxStart;
+        while (endIndex > startIndex) {
+            pivot = processPartition(numbers, startIndex, endIndex, pivot);
+            auxStart = startIndex + 1;
+
+            quickSortTailRecursion(numbers, startIndex, pivot - 1);
+
+            startIndex = auxStart;
+            pivot = endIndex;
+        }
+    }
+
+    private static class Interval {
+        final int start;
+        final int end;
+        final int pivot;
+
+        private Interval(int start, int end, int pivot) {
+            this.start = start;
+            this.end = end;
+            this.pivot = pivot;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + start + ", " + end + "] | " + pivot;
+        }
+    }
+
+    private static void quickSortNoRecursion(long[] numbers) {
+        quickSortNoRecursion(numbers, 0, numbers.length - 1);
+    }
+
+    private static void quickSortNoRecursion(long[] numbers, int startIndex, int endIndex) {
+        int newPivot = choosePivot(startIndex, endIndex);
+        Stack<Interval> intervals = new Stack<>();
+        intervals.push(new Interval(startIndex, endIndex, newPivot));
+        while (intervals.size() > 0) {
+            Interval interval = intervals.pop();
+            newPivot = processPartition(numbers, interval.start, interval.end, interval.pivot);
+
+            if (interval.start < newPivot && interval.end > newPivot) {
+                intervals.push(new Interval(interval.start, newPivot, choosePivot(interval.start, newPivot)));
+                intervals.push(new Interval(newPivot + 1, interval.end, choosePivot(newPivot + 1, interval.end)));
+            }
         }
     }
 
