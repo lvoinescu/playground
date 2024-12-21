c71e59727b09b36dc84214f2e4e920dc3f4e4114
lvoinescu <voinescu_lucian@yahoo.com>
2024-12-21 10:02:52 +0200

    Add numbers as linked-list digits

--- src/org/sam/playground/removeelementsinplace/RemoveElementsInPlace.java
+++ src/org/sam/playground/removeelementsinplace/RemoveElementsInPlace.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 public class RemoveElementsInPlace {
 
+    /**
+     * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+     * The order of the elements may be changed. Then return the number of elements in nums which
+     * are not equal to val.
+     */
     public static void main(String[] args) {
 
         var arr = new int[]{2, 3, 3, 2, 2, 7, 2};
